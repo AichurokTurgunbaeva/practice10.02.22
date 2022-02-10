e54fe47dec2504a273ff9a97cbb327c05bdb6abc
Notnik_KG <tasnim0800@gmail.com>
2022-02-10 12:48:33 +0600

    changed

--- src/com/company/people/Main.java
+++ src/com/company/people/Main.java
@@ -9,21 +9,21 @@ public class Main {
         programmer.setName("Aichurok");
         programmer.setAge(28);
         programmer.setDesignation("Back-end developer");
-        System.out.println(programmer.getName()+" "+programmer.getAge()+" "+programmer.getDesignation()+". "
+        System.out.println(programmer.getName()+" "+programmer.getAge()+" years old. "+programmer.getDesignation()+". "
                 +programmer.toString());
 
         Doctor doctor = new Doctor();
         doctor.setName("Ayzirek");
         doctor.setAge(24);
         doctor.setDesignation("Pediatrician");
-        System.out.println(doctor.getName()+" "+doctor.getAge()+" "+doctor.getDesignation()+". "
+        System.out.println(doctor.getName()+" "+doctor.getAge()+" years old. "+doctor.getDesignation()+". "
                 +doctor.toString());
 
         Musician musician = new Musician();
         musician.setName("Yanni");
         musician.setAge(67);
         musician.setDesignation("Piano player");
-        System.out.println(musician.getName()+" "+musician.getAge()+" "+musician.getDesignation()+". "
+        System.out.println(musician.getName()+" "+musician.getAge()+" years old. "+musician.getDesignation()+". "
         + musician.toString());
 
 
